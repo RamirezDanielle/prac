a9d11cc1df72bd0441fc2d5f1ed7a16b925ee1be
“Danielle <ramirezdanielle88@gmail.com>
2024-01-03 11:04:51 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+// const x = 2;
+// let y = 4;
+
+// function update(arg) {
+//   return y = 2 +(6 + Math.random());
+// }
+
+
+
+// const result = update(x);
+// console.log(result);
+
+// const x = 2;
+// let y = 4;
+
+// function update(arg) {
+//   return Math.random() + 5 + y * arg; 
+// }
+
+// y = 2;
+
+// const result = update(x);
+// console.log(result);
+
+// const x = 2;
+// let y = 4;
+
+// function update(arg) {
+//   return Math.random() + 5 + y * arg; // Ensure the result is always between 6 and 7
+// }
+
+// y = 2;
+
+// const result = update(x);
+// console.log(result);
+
+// const x = 2;
+// let y = 4;
+
+// function update(arg) {
+//   return Math.random() + 5 + y * arg - 0.5; // Ensure the result is always between 6 and 7
+// }
+
+// y = 2;
+
+// const result = update(x);
+// console.log(result);
+
+// const x = 2;
+// let y = 4;
+
+// function update(arg) {
+//   return Math.random() + 5.5 + y * arg; // Ensure the result is always between 6 and 7
+// }
+
+// y = 6;
+
+// const result = update(x);
+// console.log(result);
+
+// let randomNumber = Math.floor(Math.random() * 6) + 1;
+// console.log(randomNumber);
+
+// const x = 2;
+// let y = 4;
+
+// function update(arg) {
+//   return 6 + Math.random() * y * arg; 
+// }
+
+// y = 2;
+
+// const result = update(x);
+// console.log(result);
+
+const x = 2;
+let y = 4;
+
+function update(arg) {
+  return 6 + Math.random() * (y - 1) * arg; 
+}
+
+y = 2;
+
+const result = update(x);
+console.log(result);
